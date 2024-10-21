932eb8ee8d04a3c0542a42207e9a55a64c4f9327
shinckel <shinckel@inceptionVirtualMachine.myguest.virtualbox.org>
2024-10-21 20:04:03 +0100

    chore: clean unused files and update wordpress download in script

--- srcs/requirements/wordpress/tools/wp_start.sh
+++ srcs/requirements/wordpress/tools/wp_start.sh
@@ -1,92 +1,60 @@
 #!/bin/sh
 
-# LOGFILE=/var/log/wp_setup.log
-
-# echo "Starting WordPress setup script v2" > $LOGFILE
-
-# # Log environment variables
-# echo "MYSQL_DATABASE: $MYSQL_DATABASE" >> $LOGFILE
-# echo "MYSQL_USER: $MYSQL_USER" >> $LOGFILE
-# echo "MYSQL_PASSWORD: $MYSQL_PASSWORD" >> $LOGFILE
-# echo "MYSQL_ROOT_PASSWORD: $MYSQL_ROOT_PASSWORD" >> $LOGFILE
-# echo "MYSQL_HOSTNAME: $MYSQL_HOSTNAME" >> $LOGFILE
-# echo "WORDPRESS_URL: $WORDPRESS_URL" >> $LOGFILE
-# echo "WORDPRESS_TITLE: $WORDPRESS_TITLE" >> $LOGFILE
-# echo "WORDPRESS_ADMIN_USER: $WORDPRESS_ADMIN_USER" >> $LOGFILE
-# echo "WORDPRESS_ADMIN_PASSWORD: $WORDPRESS_ADMIN_PASSWORD" >> $LOGFILE
-# echo "WORDPRESS_ADMIN_EMAIL: $WORDPRESS_ADMIN_EMAIL" >> $LOGFILE
-
-# echo "Downloading WordPress" >> $LOGFILE
-# wget http://wordpress.org/latest.tar.gz >> $LOGFILE 2>&1
-# tar xfz latest.tar.gz >> $LOGFILE 2>&1
-# mv wordpress/* . >> $LOGFILE 2>&1
-# rm -rf latest.tar.gz wordpress >> $LOGFILE 2>&1
-
-# # Replace placeholders in wp-config-sample.php with environment variables
-# echo "Configuring wp-config.php" >> $LOGFILE
-# sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php >> $LOGFILE 2>&1
-# sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php >> $LOGFILE 2>&1
-# sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php >> $LOGFILE 2>&1
-# sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php >> $LOGFILE 2>&1
-# cp wp-config-sample.php wp-config.php >> $LOGFILE 2>&1
-
-# # Log the contents of wp-config.php
-# echo "Contents of wp-config.php:" >> $LOGFILE
-# cat wp-config.php >> $LOGFILE
-
-# # Set site URL and title if provided
-# echo "Installing WordPress" >> $LOGFILE
-# wp core install --url=$WORDPRESS_URL --title="$WORDPRESS_TITLE" \
-#     --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD \
-#     --admin_email=$WORDPRESS_ADMIN_EMAIL >> $LOGFILE 2>&1
-
-# echo "WordPress setup script completed" >> $LOGFILE
-
 LOGFILE=/var/log/wp_setup.log
 
 echo "Starting WordPress setup script v2" > $LOGFILE
 
 # Log environment variables
-echo "MYSQL_DATABASE: $MYSQL_DATABASE" >> $LOGFILE
-echo "MYSQL_USER: $MYSQL_USER" >> $LOGFILE
-echo "MYSQL_PASSWORD: $MYSQL_PASSWORD" >> $LOGFILE
-echo "MYSQL_ROOT_PASSWORD: $MYSQL_ROOT_PASSWORD" >> $LOGFILE
-echo "MYSQL_HOSTNAME: $MYSQL_HOSTNAME" >> $LOGFILE
-echo "WORDPRESS_URL: $WORDPRESS_URL" >> $LOGFILE
-echo "WORDPRESS_TITLE: $WORDPRESS_TITLE" >> $LOGFILE
-echo "WORDPRESS_ADMIN_USER: $WORDPRESS_ADMIN_USER" >> $LOGFILE
-echo "WORDPRESS_ADMIN_PASSWORD: $WORDPRESS_ADMIN_PASSWORD" >> $LOGFILE
-echo "WORDPRESS_ADMIN_EMAIL: $WORDPRESS_ADMIN_EMAIL" >> $LOGFILE
-echo "WORDPRESS_USER: $WORDPRESS_USER" >> $LOGFILE
-echo "WORDPRESS_PASSWORD: $WORDPRESS_PASSWORD" >> $LOGFILE
-echo "WORDPRESS_USER_EMAIL: $WORDPRESS_USER_EMAIL" >> $LOGFILE
+{
+    echo "MYSQL_DATABASE: $MYSQL_DATABASE"
+    echo "MYSQL_USER: $MYSQL_USER"
+    echo "MYSQL_PASSWORD: $MYSQL_PASSWORD"
+    echo "MYSQL_ROOT_PASSWORD: $MYSQL_ROOT_PASSWORD"
+    echo "MYSQL_HOSTNAME: $MYSQL_HOSTNAME"
+    echo "WORDPRESS_URL: $WORDPRESS_URL"
+    echo "WORDPRESS_TITLE: $WORDPRESS_TITLE"
+    echo "WORDPRESS_ADMIN_USER: $WORDPRESS_ADMIN_USER"
+    echo "WORDPRESS_ADMIN_PASSWORD: $WORDPRESS_ADMIN_PASSWORD"
+    echo "WORDPRESS_ADMIN_EMAIL: $WORDPRESS_ADMIN_EMAIL"
+    echo "WORDPRESS_USER: $WORDPRESS_USER"
+    echo "WORDPRESS_PASSWORD: $WORDPRESS_PASSWORD"
+    echo "WORDPRESS_USER_EMAIL: $WORDPRESS_USER_EMAIL"
+} >> $LOGFILE
 
+# Clean previous installations
+echo "Cleaning previous installations" >> $LOGFILE
+rm -rf wp-admin wp-content wp-includes latest.tar.gz wordpress >> $LOGFILE 2>&1
+
+# Download WordPress
 echo "Downloading WordPress" >> $LOGFILE
-wget http://wordpress.org/latest.tar.gz >> $LOGFILE 2>&1
-tar xfz latest.tar.gz >> $LOGFILE 2>&1
-
-# Move WordPress files if the target directories are empty
-if [ ! "$(ls -A wp-admin)" ] && [ ! "$(ls -A wp-content)" ] && [ ! "$(ls -A wp-includes)" ]; then
-    mv wordpress/* . >> $LOGFILE 2>&1
+if wget https://wordpress.org/latest.tar.gz >> $LOGFILE 2>&1; then
+    echo "Download successful" >> $LOGFILE
 else
-    echo "Target directories are not empty, skipping move" >> $LOGFILE
+    echo "Download failed" >> $LOGFILE
 fi
 
+# Extract WordPress
+echo "Extracting WordPress" >> $LOGFILE
+tar xfz latest.tar.gz >> $LOGFILE 2>&1
+
+# Move WordPress files
+echo "Moving WordPress files" >> $LOGFILE
+mv wordpress/* . >> $LOGFILE 2>&1
 rm -rf latest.tar.gz wordpress >> $LOGFILE 2>&1
 
-# Replace placeholders in wp-config-sample.php with environment variables
+# Configure wp-config.php
 echo "Configuring wp-config.php" >> $LOGFILE
-sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php >> $LOGFILE 2>&1
-sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php >> $LOGFILE 2>&1
-sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php >> $LOGFILE 2>&1
-sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php >> $LOGFILE 2>&1
 cp wp-config-sample.php wp-config.php >> $LOGFILE 2>&1
+sed -i "s/username_here/$MYSQL_USER/g" wp-config.php >> $LOGFILE 2>&1
+sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config.php >> $LOGFILE 2>&1
+sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config.php >> $LOGFILE 2>&1
+sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config.php >> $LOGFILE 2>&1
 
 # Log the contents of wp-config.php
 echo "Contents of wp-config.php:" >> $LOGFILE
 cat wp-config.php >> $LOGFILE
 
-# Set site URL and title if provided
+# Install WordPress
 echo "Installing WordPress" >> $LOGFILE
 if wp core install --url=$WORDPRESS_URL --title="$WORDPRESS_TITLE" \
     --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD \
